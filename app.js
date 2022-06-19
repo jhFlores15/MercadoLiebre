cb50979f2bf8735c49c15668332d8c179cb3a24f
jflores <johflores1510@gmail.com>
2022-06-19 17:43:53 -0400

    mercadoLiebre ok

--- app.js
+++ app.js
@@ -9,8 +9,17 @@ app.listen(3030,() => console.log("Servidor escuchando en puerto 3030"));
 app.get('/', (req,res) => { 
     let index= path.resolve(__dirname,"./views/home.html")
     res.sendFile (index);
-  })
+})
 
+app.get('/register', (req,res) => { 
+  let index= path.resolve(__dirname,"./views/register.html")
+  res.sendFile (index);
+})
+
+app.get('/login', (req,res) => { 
+  let index= path.resolve(__dirname,"./views/login.html")
+  res.sendFile (index);
+})
  
 
 
