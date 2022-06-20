e3ca89d2b97ad0e76252881530f4273661fa19c2
jflores <johflores1510@gmail.com>
2022-06-20 19:58:29 -0400

    heroku

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const app = express();
 const path = require('path');
 
 const publicPath = path.resolve(__dirname, './public');
+
 app.use(express.static(publicPath));
-app.listen(3030,() => console.log("Servidor escuchando en puerto 3030"));
+app.listen(process.env.PORT || 3000,() => console.log("Servidor escuchando en puerto 3000"));
 
 app.get('/', (req,res) => { 
     let index= path.resolve(__dirname,"./views/home.html")
